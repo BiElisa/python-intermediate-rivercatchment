f40af22fec4846af33d9b46cc691544864f297fe
YongchaoHuang <huangyongchao2012@gmail.com>
2023-03-30 15:16:12 +0100

    Yong's real changes

--- catchment/views.py
+++ catchment/views.py
@@ -23,3 +23,24 @@ def visualize(data_dict):
     fig.tight_layout()
 
     plt.show()
+
+# visulise data using box plot
+def boxplot(data_dict):
+    """Display box plots of the given data.
+
+    :param data_dict: Dictionary of name -> data to plot
+    """
+
+    num_plots = len(data_dict)
+    fig = plt.figure(figsize=((3 * num_plots) + 1, 3.0))
+
+    for i, (name, data) in enumerate(data_dict.items()):
+        axes = fig.add_subplot(1, num_plots, i + 1)
+
+        axes.set_ylabel(name)
+        axes.boxplot(data)
+        axes.legend(data.columns)
+
+    fig.tight_layout()
+
+    plt.show()
